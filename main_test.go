c673d5b45125a984d22db9c7b6e82320d7bca3a2
Karl Matthias <karl.matthias@gonitro.com>
2017-01-11 10:57:47 +0000

    Add tests for SetProcessName()

--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -261,6 +262,22 @@ func Test_watchContainer(t *testing.T) {
 			err := <-resultChan
 			So(err, ShouldNotBeNil)
 			So(err.Error(), ShouldContainSubstring, "Container deadbeef0010 not running!")
+		})
+	})
+}
+
+func Test_SetProcessName(t *testing.T) {
+	Convey("Setting the process name", t, func() {
+		originalLen := len(os.Args[0])
+
+		Convey("bounds the name length to existing length", func() {
+			SetProcessName(strings.Repeat("x", originalLen + 10))
+			So(len(os.Args[0]), ShouldEqual, originalLen)
+		})
+
+		Convey("modifies ARGV[0] correctly and pads name", func() {
+			SetProcessName("decameron")
+			So(os.Args[0], ShouldResemble, "decameron" + strings.Repeat(" ", originalLen - len("decameron")))
 		})
 	})
 }
